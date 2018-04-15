7527c24b4288b63236163a2c0adba92b6d50184f
RJoshima <sojoy82@yahoo.co.jp>
2018-04-16 02:19:51 +0900

    add: truffle-contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,5 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = process.env.ROPSTEN_MNEMONIC;
-const accessToken = process.env.INFURA_ACCESS_TOKEN;
-
-// when you develop, use "truffle develp" in command line (This does not mean you use development network below)
-// when you use testnet below, use "truffle migrate --network ropsten" in command
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // match any network
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          mnemonic,
-          "https://ropsten.infura.io/" + accessToken
-        );
-      },
-      network_id: 3,
-      gas: 3000000
-    }
-  }
-}
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+};
+
--- truffle.js
+++ truffle.js
@@ -1,4 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env.ROPSTEN_MNEMONIC;
+const accessToken = process.env.INFURA_ACCESS_TOKEN;
+
+// when you develop, use "truffle develp" in command line (This does not mean you use development network below)
+// when you use testnet below, use "truffle migrate --network ropsten" in command
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // match any network
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 3000000
+    }
+  }
+}
